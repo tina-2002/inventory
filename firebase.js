90b7da6a61fd864a671677a5889be87581ca1117
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-08-07 19:08:21 -0400

    chore: Update Firebase configuration to use environment variables

--- firebase.js
+++ firebase.js
@@ -1,13 +1,23 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
- apiKey: "YOUR_API_KEY",
- authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
- projectId: "YOUR_PROJECT_ID",
- storageBucket: "YOUR_PROJECT_ID.appspot.com",
- messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
- appId: "YOUR_APP_ID"
- };
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
+// Insert this into .env file with no quotation marks:
+// NEXT_PUBLIC_FIREBASE_API_KEY=your_api_key
+// NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=your_auth_domain
+// NEXT_PUBLIC_FIREBASE_PROJECT_ID=your_project_id
+// NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=your_storage_bucket
+// NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=your_messaging_sender_id
+// NEXT_PUBLIC_FIREBASE_APP_ID=your_app_id
+
+
+
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 export { firestore };
